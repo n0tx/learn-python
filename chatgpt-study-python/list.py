0a71397fb9e8f00f7c3e038ae5c7323db7c19997
n0tx <rcandra91@msn.com>
2025-09-25 13:13:52 +0700

    list

---
+++ chatgpt-study-python/list.py
@@ -0,0 +1,15 @@
+buah = ["apel", "pisang", "jeruk"]
+print(buah)
+print("")
+
+print("menambahakan buah mangga")
+buah.append("mangga")
+print(buah)
+print("")
+
+print("akses item pertama dari list")
+print(buah[0])
+print("")
+
+print("cetak panjang list")
+print(len(buah))
