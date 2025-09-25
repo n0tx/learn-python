824bc4dea4cc75163d12fae3233225cf5ed4c207
n0tx <rcandra91@msn.com>
2025-09-25 11:27:12 +0700

    variabel, tipe dan konversi tipe data

---
+++ chatgpt-study-python/var_tipe.py
@@ -0,0 +1,29 @@
+print("# Integer")
+x = 5
+print(x)
+print("")
+
+print("# String")
+nama = "Riki"
+print(nama)
+print("")
+
+print("# Float")
+pi = 3.14
+print(pi)
+print("")
+
+print("# Boolean")
+is_active = True
+print(is_active)
+print("")
+
+print("# List")
+buah = ["apel", "pisang", "jeruk"]
+print(buah)
+print("")
+
+print("# Dictionary")
+user = {"nama": "Riki", "usia": 25}
+print(user)
+print("")
